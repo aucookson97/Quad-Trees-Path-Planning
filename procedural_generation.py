b90e68b46859806c7b637421542ea591f0b25f34
Aidan Cookson <aidanucookson@gmail.com>
2020-11-21 21:03:35 -0500

    Fixed Merge Conflicts

--- procedural_generation.py
+++ procedural_generation.py
@@ -21,23 +21,15 @@ class IslandGenerator():
         self.noise_arr[self.noise_arr >= threshold] = 1
         self.noise_arr[self.noise_arr < threshold] = 0
         self.map = self.noise_arr.astype(int) # 1s are land, 0s are water
-        self.map = np.repeat(self.map[...,np.newaxis],repeats=3,axis=2)
+        self.map = np.repeat(self.map[...,np.newaxis], repeats=3, axis=2)
 
         # Create an image for visual purposes. TODO: Do with numpy
-<<<<<<< HEAD
         img = np.zeros((self.map_size, self.map_size, 3))
-        for y in range(self.map_size):
-            for x in range(self.map_size):
-                img[y, x] = LAND if self.map[y, x] else WATER
+        img = np.multiply(self.map,LAND) + np.multiply(np.logical_not(self.map),WATER)
         self.img = Image.fromarray(img.astype('uint8'), 'RGB')
-    
-    def _perlin_noise(self, scale=.2, octaves=3, persistence=.5, lacunarity=2.0):
-=======
-        self.img = np.zeros((self.map_size, self.map_size, 3))
-        self.img = np.multiply(self.map,LAND) + np.multiply(np.logical_not(self.map),WATER)
 
-    def _perlin_noise(self, scale=.2, octaves=3, persistence=3, lacunarity=2.0):
->>>>>>> beb9b5ace8f6644bc7f54fe70417b08d241d2df5
+
+    def _perlin_noise(self, scale=.2, octaves=2, persistence=3, lacunarity=2.0):
         seed = np.random.randint(0, 100)
         noise_arr = np.zeros((self.map_size,self.map_size))
         
@@ -55,27 +47,5 @@ class IslandGenerator():
                                                 base=seed)
         return noise_arr
         
-<<<<<<< HEAD
     def show_map(self):
         self.img.show()
-        
-    
-    
-# ig = IslandGenerator(100, .1)
-# mapp = ig.map
-# img = ig.img
-# ig._show_map()
-=======
-    def _show_map(self):
-        img_map = Image.fromarray(self.img.astype('uint8'), 'RGB')
-        img_map.show()
-        return img_map
-        
-    
-    
-ig = IslandGenerator(1000, .05)
-mapp = ig.map
-img = ig.img
-img_map = ig._show_map()
-img_map.save('example.png')
->>>>>>> beb9b5ace8f6644bc7f54fe70417b08d241d2df5
